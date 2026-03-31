de2bd0ac428f1a6d90c71bae8fa4e5c948cfa1fd
사라 <prinsaraouo@gmail.com>
2026-04-01 04:29:50 +0900

    카테고리별 객체 생성, 커머스시스템 내 속,생까지 완성

---
+++ src/project/CommerceSystem.java
@@ -0,0 +1,38 @@
+package project;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class CommerceSystem {
+
+    //설명 : Product 클래스를 관리하는 클래스입니다.
+    //전자제품, 의류, 식품 등 각 카테고리 내에 여러 Product를 만들어 줍니다. (완료)
+    //main 함수에서 관리하던 입력과 반복문 로직은 이제 start 함수를 만들어 관리합니다.
+    //List<Product> products 는 CommerceSystem 클래스 생성자를 통해 값을 할당합니다.
+    //CommerceSystem 객체를 생성하고 사용하는 main 함수에서 객체를 생성할 때 값을 넘겨줍니다.
+
+    //반복문을 활용해 products를 탐색하면서 하나씩 접근합니다.
+
+    //속성
+    private List<Product> productList;
+    private Scanner sc;
+
+
+    //생성자
+
+    /**
+     *
+     * @param productList Main에서 쓰는 ProductList 가져오기
+     * @param sc Scanner / 입력
+     */
+
+    public CommerceSystem(List<Product> productList, Scanner sc) {
+        this.productList = productList;
+        this.sc = sc;
+    }
+
+
+    //기능
+
+
+}
--- src/project/Main.java
+++ src/project/Main.java
@@ -8,41 +8,51 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //Scanner 선언
-        Scanner sc = new Scanner(System.in);
-
         //전자제품 목록 객체
         Product galaxy = new Product("Galaxy S25", 1200000, "최신 안드로이드 스마트폰", 100);
         Product iPhone = new Product("iPhone 16", 1350000, "Apple의 최신 스마트폰", 10000);
         Product macBook = new Product("MacBook Pro", 2400000,"M3 칩셋이 탑재된 노트북", 5959);
         Product airPods = new Product("AirPods Pro", 350000, "노이즈 캔슬링 무선 이어폰", 1004);
 
+        //의류 목록 객체
+        Product dior = new Product("빤짝이 디올 상의", 500000000, "사라 디자이너가 한땀한땀 만든 상의", 112);
+        Product adidas = new Product("훔쳐온 짝퉁 아디다스 양말", 99999999, "사라 디자이너가 훔친 짝퉁 아디다스 양말", 1);
+        Product chanel = new Product("무지개 샤넬 장갑", 1010101010, "사라 디자이너가 하늘로 올라가 직접 만든 무지개 장갑", 5252);
+        Product celine = new Product("오징어 먹물 셀린느 하의", 58391823, "사라 디자이너와 그녀의 친구 김오징어씨의 합작 하의", 119);
 
-        List<Product> productList = new ArrayList<>(List.of(galaxy, iPhone, macBook, airPods));
+        //음식 목록 객체
+        Product candy = new Product("휴지맛 캔디", 999999, "사라의 최애 간!식!", 1);
+        Product onion = new Product("나무에서 자란 양파", 1, "사라가 직접 나무에서 발견한 양파다!! 초희귀!", 9999);
+        Product apple = new Product("화성에서 가져온 사과", 99999999, "사라가 직접 화성에서 발견한 사과다. 아주 희귀", 99999999);
+        Product tylenol = new Product("약이 아닌 식품 타이레놀", 10, "두통에 직빵이다. 맛은 없다.", 119);
 
-        //반복문을 활용해 products를 탐색하면서 하나씩 접근합니다.
 
-        while (true) {
-            int productButton = 1;
+        List<Product> eletronicList = new ArrayList<>(List.of(galaxy, iPhone, macBook, airPods));
+        List<Product> clothingList = new ArrayList<>(List.of(dior, adidas, chanel, celine));
+        List<Product> foodList = new ArrayList<>(List.of(candy, onion, apple, tylenol));
 
-            System.out.println("[ 실시간 커머스 플랫폼 - 전자제품 ]");
-
-            for(Product product : productList) {
-                System.out.println(productButton + ". "
-                        + product.getName() + " | "
-                        + String.format("%,d원", product.getPrice()) + " | "
-                        + product.getExplain());
-                productButton++;
-            }
-
-            //종료 구현
-            System.out.println("0. 종료         | 프로그램 종료 ");
-
-            int exit = sc.nextInt();
-            if (exit == 0) {
-                System.out.println("커머스 플랫폼을 종료합니다.");
-                break;
-            }
-        }
     }
 }
+
+//        while (true) {
+//            int productButton = 1;
+//
+//            System.out.println("[ 실시간 커머스 플랫폼 - 전자제품 ]");
+//
+//            for(Product product : eletronicList) {
+//                System.out.println(productButton + ". "
+//                        + product.getName() + " | "
+//                        + String.format("%,d원", product.getPrice()) + " | "
+//                        + product.getExplain());
+//                productButton++;
+//            }
+//
+//            //종료 구현
+//            System.out.println("0. 종료         | 프로그램 종료 ");
+//
+//            int exit = sc.nextInt();
+//            if (exit == 0) {
+//                System.out.println("커머스 플랫폼을 종료합니다.");
+//                break;
+//            }
+//        }
