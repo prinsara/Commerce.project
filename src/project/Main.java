b1c8798e1163a0b2969c62dea763d9f10fd7dff8
사라 <prinsaraouo@gmail.com>
2026-04-01 03:25:16 +0900

    STEP1 Main Class에 상품 목록 추가

--- src/project/Main.java
+++ src/project/Main.java
@@ -1,4 +1,18 @@
 package project;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        //main 함수에서 Product 클래스를 생성하여 상품 목록을 추가합니다.
+        //Product 객체 생성을 통해 상품명, 가격, 설명, 재고수량을 세팅합니다.
+        //키워드: new
+
+        Product galaxy = new Product("Galaxy S25", 1200000, "최신 안드로이드 스마트폰", 100);
+        Product iPhone = new Product("iPhone 16", 1350000, "Apple의 최신 스마트폰", 10000);
+        Product macBook = new Product("MacBook Pro", 2400000,"M3 칩셋이 탑재된 노트북", 5959);
+        Product airPods = new Product("AirPods Pro", 350000, "노이즈 캔슬링 무선 이어폰", 1004);
+
+
+    }
 }
--- src/project/Product.java
+++ src/project/Product.java
@@ -1,4 +1,32 @@
 package project;
 
 public class Product {
+
+    //설명 : 개별 상품을 관리하는 클래스입니다. 현재는 전자제품만 관리합니다.
+
+    /**
+     * 상품명, 가격, 설명, 재고수량 필드 생성
+     */
+
+    private String name;
+    private int price;
+    private String explain;
+    private int inventory;
+
+    /**
+     *
+     * @param name 이름
+     * @param price 가격
+     * @param explain 설명
+     * @param inventory 재고수량
+     */
+    //생성자
+    public Product(String name, int price, String explain, int inventory) {
+        this.name = name;
+        this.price = price;
+        this.explain = explain;
+        this.inventory = inventory;
+    }
+
+    //기능
 }
