a718682478a62c8b7767137bed2b549b118ff58f
사라 <prinsaraouo@gmail.com>
2026-04-01 17:50:34 +0900

    STEP2 상품 리스트 출력 while 추가하여 상품 리스트 반복

--- src/project/CommerceSystem.java
+++ src/project/CommerceSystem.java
@@ -44,33 +44,44 @@ public class CommerceSystem {
             // 2. 카테고리 번호 입력 값 받기
             System.out.println("0. 종료    |  프로그램 종료");
             int categoryInput = sc.nextInt();
-            if (categoryInput == 0) {
+
+            //입력 범위, 종료
+            if (categoryInput < 0 || categoryInput > categoryList.size()) {
+                System.out.println("범위 내에서만 선택 가능합니다. 다시 입력해주세요.");
+                continue;
+            } else if (categoryInput == 0) {
                 System.out.println("커머스 플랫폼을 종료합니다.");
                 break;
             }
 
-            // 3. 카테고리 내 맞는 카테고리 상품 보여주기
-            Category selectedCategory = categoryList.get(categoryInput - 1);
+            // 3. 카테고리 입력 값에 해당하는 카테고리 번호 찾기
+            Category findCategoryList = categoryList.get(categoryInput - 1);
 
-            int productButton = 1;
+            // 4. 해당 카테고리 내에 있는 List 가져오기
+            List<Product> productList = findCategoryList.getProductList();
 
-            List<Product> productList = selectedCategory.getProductList();
 
-            System.out.println("[ " + selectedCategory.getCategoryName() + " 카테고리 ]");
-            for (Product product : productList) {
-                System.out.println(productButton + ". " + product.getName() + " | "
-                        + String.format("%,d원", product.getPrice()) + " | "
-                        + product.getExplain());
-                productButton++;
+            while (true) {
+                // 5. List를 사용자에게 보여주기
+                int productButton = 1;
+
+                for (Product product : productList) {
+                    System.out.println(productButton + ". " + product.getName() + " | " + String.format("%,d원", product.getPrice()) + " | " + product.getExplain());
+                    productButton++;
+                }
+
+                // 6. 입력 버튼
+                System.out.println("0. 뒤로가기");
+                int productInput = sc.nextInt();
+
+                // 7. 범위
+                if (productInput < 0 || productInput > productList.size()) {
+                    System.out.println("범위 내에서만 선택 가능합니다. 다시 입력해주세요.");
+                    continue;
+                } else if (productInput == 0) {
+                    break;
+                }
             }
-
-
-            System.out.println("0. 뒤로가기");
-            int productInput = sc.nextInt();
-            if (productInput == 0) {
-                continue;
-            }
-
         }
     }
 }
