453b93ed172978422a1d95196154735efb5ce2c3
사라 <prinsaraouo@gmail.com>
2026-04-01 19:49:42 +0900

    feat : Customer 클래스 생성 후 생성자까지 완료

---
+++ src/project/Customer.java
@@ -0,0 +1,14 @@
+package project;
+
+public class Customer {
+
+    private String name;
+    private String email;
+    private String grade;
+
+    public Customer(String name, String email, String grade) {
+        this.name = name;
+        this.email = email;
+        this.grade = grade;
+    }
+}
